7a28e04b4fb669c84e9d596d4d3d3706356ea197
Pavan Kumar <pavan.kumar@mahoujas.com>
2015-07-27 00:09:39 +0530

    Initial working code

---
+++ lib/smsowl.rb
@@ -0,0 +1,73 @@
+require 'net/http'
+require 'json'
+
+ENV['SSL_CERT_FILE'] = './cacert.pem'
+
+class SmsType
+	NORMAL = "normal"
+	FLASH = "flash"
+end
+
+
+class SmsOwl
+
+	URL = URI('https://api.smsowl.in/v1/sms');
+
+	def initialize(accountId,apiKey)
+		@accountId = accountId
+		@apiKey = apiKey
+	end
+
+	def sendPromotionalSms(senderId,to,message,smsType = 'normal')
+		req = Net::HTTP::Post.new(SmsOwl::URL, initheader = {'Content-Type' =>'application/json'})
+		data = getCommonArray("promotional",smsType,senderId,to)
+		data['message'] = message
+		req.body = data.to_json
+		res = Net::HTTP.start(URL.hostname, URL.port,
+			:use_ssl => URL.scheme == 'https') do |http|
+			http.request(req)
+		end
+		if res.code == '200'
+			jsonRes = JSON.parse(res.body)
+			if to.kind_of?(Array)
+				return jsonRes["smsIds"]
+			else
+				return jsonRes["smsId"]
+			end
+		elsif res.code == '400'
+			jsonRes = JSON.parse(res.body)
+			raise jsonRes['message']
+		end
+	end
+
+	def sendTransactionalSms(senderId,to,templateId,placeholderArray)
+		req = Net::HTTP::Post.new(SmsOwl::URL, initheader = {'Content-Type' =>'application/json'})
+		data = getCommonArray("transactional","normal",senderId,to)
+		data['templateId'] = templateId
+		data['placeholders'] = placeholderArray
+		req.body = data.to_json
+		res = Net::HTTP.start(URL.hostname, URL.port,
+			:use_ssl => URL.scheme == 'https') do |http|
+			http.request(req)
+		end
+		if res.code == '200'
+			jsonRes = JSON.parse(res.body)
+			return jsonRes["smsId"]
+		elsif res.code == '400'
+			jsonRes = JSON.parse(res.body)
+			raise jsonRes['message']
+		end
+	end
+
+
+	def getCommonArray(dndType,smsType,senderId,to)
+		return {   	
+    		accountId: @accountId,
+	    	apiKey: @apiKey,
+	    	dndType: dndType,
+	    	smsType: smsType,
+	    	senderId: senderId,
+	    	to: to
+	    }
+	end
+end
---
+++ smsowl.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name        = 'smsowl'
+  s.version     = '1.0.0'
+  s.date        = '2015-07-26'
+  s.summary     = "This is Ruby wrapper for SmsOwl REST API"
+  s.description = "This is Ruby wrapper for SmsOwl REST API"
+  s.authors     = ["Mahoujas"]
+  s.email       = 'mahoujas@mahoujas.com'
+  s.files       = ["lib/smsowl.rb"]
+  s.homepage    = 'https://smsowl.in'
+  s.license       = 'MIT'
+end
